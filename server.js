09e6ce8dca2891e7578e2918620619b5c76e7a6b
paul-tanase <paul_tanase2@yahoo.ca>
2014-12-04 23:23:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var express = require('express');
+/*var express = require('express');
 var app = express(), server = require('http').createServer(app);
 app.use('/', express.static('./'));
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8000
 , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-server.listen(port, ip);
+server.listen(port, ip);*/
+
+var http = require('http');
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    port = process.env.OPENSHIFT_NODEJS_PORT || '8080';
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, ip);
+console.log('Server running at http://'+ip+':'+port+'/');
